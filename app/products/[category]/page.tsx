55dd8eb22295009e6d2f9a0aa5afafc97e624599
MuharremGonel <muharremgonel20@gmail.com>
2024-11-15 15:26:54 +0300

    categories and search pages added

---
+++ app/products/[category]/page.tsx
@@ -0,0 +1,21 @@
+import Products from "@/components/Products";
+import Sidebar from "@/components/Sidebar";
+interface categoryParams {
+  params: {
+    category: string;
+  };
+}
+const Categories: React.FC<categoryParams> = async ({ params }) => {
+  const { category } = params;
+  return (
+    <div className="max-w-7xl py-20 mx-auto flex gap-5">
+      <aside className="max-w-xs w-full hidden xl:block">
+        <Sidebar />
+      </aside>
+      <main className="w-full">
+        <Products fetchPath={`${process.env.NEXT_PUBLIC_API_URL}/products/category/${category}?`} />
+      </main>
+    </div>
+  );
+};
+export default Categories;
--- components/Products.tsx
+++ components/Products.tsx
@@ -1,61 +1,60 @@
 "use client";
 import React, { useState, useEffect, useRef, useCallback } from "react";
 import ProductCard from "./ProductCard";
-import { Product } from "../types/types";
+import { Product } from "@/types/types";
+import Link from "next/link";
 
-const Products = () => {
+interface ProductsProps {
+  fetchPath: string;
+}
+
+const Products: React.FC<ProductsProps> = ({ fetchPath }) => {
   const [productItems, setProductItems] = useState<Product[]>([]);
-  const [skip, setSkip] = useState(0); // Keep track of the skip value for pagination
-  const [hasMore, setHasMore] = useState(true); // Flag to check if there are more products to load
-  const [isLoading, setIsLoading] = useState(false); // Loading state
-  const [totalProducts, setTotalProducts] = useState(0); // Total number of products
+  const [skip, setSkip] = useState(0);
+  const [hasMore, setHasMore] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const [totalProducts, setTotalProducts] = useState(0);
 
 
-  const limit = 6; // Limit per request
-  const loadMoreButtonRef = useRef<HTMLButtonElement>(null); // Reference for the "Load More" button
+  const limit = 6;
+  const loadMoreButtonRef = useRef<HTMLButtonElement>(null);
 
 
   const fetchProducts = useCallback(async () => {
-    if (isLoading || !hasMore) return; // Prevent multiple fetches if already loading or no more products
+    if (isLoading || !hasMore) return;
 
 
     setIsLoading(true);
 
     try {
-      const response = await fetch(
-        `${process.env.NEXT_PUBLIC_API_URL}/products?limit=${limit}&skip=${skip}`
-      );
+      const response = await fetch(`${fetchPath}limit=${limit}&skip=${skip}`);
       const data = await response.json();
       const newProducts = data.products;
       setTotalProducts(data.total);
 
       setProductItems((prevProducts) => [...prevProducts, ...newProducts]);
-      setHasMore(newProducts.length === limit); // If fewer than `limit`, no more products
-      setSkip((prevSkip) => prevSkip + limit); // Increment skip by the limit to fetch the next set
+      setHasMore(newProducts.length === limit);
+      setSkip((prevSkip) => prevSkip + limit);
     } catch (error) {
       console.error("Error fetching products:", error);
     }
 
 
     setIsLoading(false);
-    //console.log("skip:", skip);
   }, [skip, isLoading, hasMore]);
 
-  // Log skip value after it has been updated
-  // useEffect(() => {
-  //   console.log("skip:", skip);
-  // }, [skip]); // Trigger the log when the `skip` state changes
+
 
   const observerCallback = (entries: IntersectionObserverEntry[]) => {
     const entry = entries[0];
     if (entry.isIntersecting && !isLoading) {
-      fetchProducts(); // Trigger fetch when the "Load More" button is visible in the viewport
+      fetchProducts();
     }
   };
 
   const observerOptions = {
-    rootMargin: "100px", // Trigger when near the bottom of the page
-    threshold: 1.0, // Trigger when fully in view
+    rootMargin: "100px",
+    threshold: 1.0,
   };
 
   useEffect(() => {
@@ -65,26 +64,26 @@ const Products = () => {
     );
 
     if (loadMoreButtonRef.current) {
-      observer.observe(loadMoreButtonRef.current); // Start observing the "Load More" button
+      observer.observe(loadMoreButtonRef.current);
     }
 
     return () => {
       if (loadMoreButtonRef.current) {
-        observer.unobserve(loadMoreButtonRef.current); // Cleanup observer when component unmounts
+        observer.unobserve(loadMoreButtonRef.current);
       }
     };
   }, [isLoading]); // Re-run observer setup when `isLoading` changes
 
-  const generateRandomNumber = () => Math.floor(Math.random() * 1000); // Random number to ensure unique keys
-
+  const generateRandomNumber = () => Math.floor(Math.random() * 1000);
   return (
     <>
       <div className="max-w-7xl flex flex-wrap gap-5 gap-y-2">
         {productItems.map((product) => (
-          <ProductCard
-            key={`${product.id}-${generateRandomNumber()}`}
-            product={product}
-          />
+          <Link key={`${product.id}-${generateRandomNumber()}`} href={`/product/${product.id}`}>
+            <ProductCard
+              product={product}
+            />
+          </Link>
         ))}
       </div>
       {productItems.length < totalProducts && (
